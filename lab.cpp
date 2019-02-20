f67e45988679aaf731ab0762630622b25b57622b
Jeremiah Munson <j.munson225@gmail.com>
2019-02-20 15:18:54 -0600

    Changed square to power function.

--- lab.cpp
+++ lab.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
-void square(int val)
+void power(int base, int exponent)
 {
-    cout << val*val << endl;
+    int val;
+    if(exponent == 0 && base != 0)
+    {
+        val = 1;
+    }
+    else{
+        val = base;
+        // Because val = base above that is the base^1 case so the for loop goes to i < (exponent - 1)
+        for(int i = 0; i < (exponent - 1); i++)
+        {
+            val*=base;
+        }
+    }
+    cout << val << endl;
 }
 
 int main(){
